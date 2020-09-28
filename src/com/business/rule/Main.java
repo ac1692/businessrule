2ccd52f1143738516c4486db3cbfe4f788a776b3
ankit chauhan <chauhan1692@gmail.com>
2020-09-28 22:15:37 +0530

    send rules

--- src/com/business/rule/Main.java
+++ src/com/business/rule/Main.java
@@ -1,8 +1,21 @@
 package com.business.rule;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> listOfRules = List.of("physical product", "book", "membership"
+                ,"upgrade","Learning to ski");
+
+        Main main = new Main();
+        System.out.println(main.getRules(listOfRules));
     }
-}
+
+    public String getRules(List<String> listOfRules) {
+        StringBuilder sb= new StringBuilder();
+        RuleFactory ruleFactory = new RuleFactory();
+        listOfRules.stream().forEach(rule->
+                ruleFactory.getRules(rule).stream().forEach(r -> sb.append(r.applyRule())));
+        return sb.toString();
+    }}
