e739cf3f1473a44cc8721e887d06bbe47efa3c08
ankit chauhan <chauhan1692@gmail.com>
2020-09-28 22:16:12 +0530

    code change

--- src/com/business/rule/Main.java
+++ src/com/business/rule/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public String getRules(List<String> listOfRules) {
         StringBuilder sb= new StringBuilder();
         RuleFactory ruleFactory = new RuleFactory();
-        listOfRules.stream().forEach(rule->
-                ruleFactory.getRules(rule).stream().forEach(r -> sb.append(r.applyRule())));
+        listOfRules.forEach(rule->
+                ruleFactory.getRules(rule).forEach(r -> sb.append(r.applyRule())));
         return sb.toString();
     }}
